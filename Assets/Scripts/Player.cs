483ad0aab4450bf0789e4b3c60e02a1b0cc69ce0
Alex Pereira Sacritán <alexpersac01@gmail.com>
2023-06-21 20:05:53 +0200

    Jump
    Detección del suelo mediante OverlapCircle
    Ajuste de gravedad y fuerzas para el movimiento

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rbPlayer;
+    [SerializeField] float speed, force;
+    float h, v;
+    [Header("CheckSphereGround")]
+    [SerializeField] Transform feets;
+    [SerializeField] LayerMask ground;
+    float radFeets = 0.5f;
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+        OnGround();
+    }
+
+    private void FixedUpdate()
+    {
+        rbPlayer.AddForce(new Vector3(h, 0, 0) * speed, ForceMode2D.Force);
+    }
+
+    void OnGround()
+    {
+        //bool onGround = Physics.CheckSphere(feets.position, radFeets, ground);
+        if (Physics2D.OverlapCircle(feets.position, radFeets, ground) && Input.GetKeyDown(KeyCode.Space))
+        {
+            rbPlayer.AddForce(Vector3.up * force, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(feets.position, radFeets);
+    }
+}
