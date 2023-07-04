01daa530ade7720e435b5e590932f8e3315d2055
Alex Pereira Sacritán <alexpersac01@gmail.com>
2023-07-04 04:06:00 +0200

    OnPointer(Enter & Exit)
    Detección cuando cada botón es seleccionado para mostrar los valores correspondientes a cada carta

---
+++ Assets/TarotButtons.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TarotButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    void Start()
+    {
+        
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+
+    }
+}
