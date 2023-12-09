0308988df699bf05e743cb43efff645be3429d5b
Alex Pereira Sacritán <alexpersac01@gmail.com>
2023-12-09 20:08:24 +0100

    Fixed errors
    No se mostraba el valor de las cartas y se empezaba con un total de 100 fragmentos de alma

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
 
     int playerLifes = 5;
     int playerSouls = 3;
-    int soulFragments = 100;
+    int soulFragments = 0;
     [Header("lista de mejoras")]
     [SerializeField] List<Sprite> upgradeList;
     List<bool> upgrades;
